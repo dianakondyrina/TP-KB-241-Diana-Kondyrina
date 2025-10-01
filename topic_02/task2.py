3745417ad2d12ae1ad8d1815f83faef44184f2b6
Diana Kondyrina <dianakondyrina@gmail.com>
2025-10-01 22:35:51 +0300

    add tasks for topic 02

--- topic_02/task2.py
+++ topic_02/task2.py
@@ -2,13 +2,12 @@ a = float(input("Напишіть перше число: "))
 b = float(input("Напишіть друге число: "))
 operation = input("Оберіть операцію: ")
 
-
 if operation == "+":
-     print(f"Результат: {a + b}")
+     print("Результат: {a + b}")
 elif operation == "-":
-    print(f"Результат: {a - b}")
+    print("Результат: {a - b}")
 elif operation == "*":
-    print(f"Результат: {a * b}")
+    print("Результат: {a * b}")
 else:
     if b == 0:
               print("на 0 не ділять")
